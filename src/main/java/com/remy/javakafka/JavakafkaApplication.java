7643eabef649a0cab3bfd04d186db8bc1d63069c
Bwamutala Miantezila <bwamutalamiantezila@MacBook-Pro.local>
2020-09-15 21:37:00 -0700

    added spring code

---
+++ src/main/java/com/remy/javakafka/JavakafkaApplication.java
@@ -0,0 +1,13 @@
+package com.remy.javakafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavakafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavakafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/remy/javakafka/JavakafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remy.javakafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavakafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
